a887bb68fe2600f83914daefabb9d59ba05b65db
john <jhopkin@protonmail.com>
2023-10-24 22:37:37 +1000

    works but needs refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use rand::Rng;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::num::IntErrorKind::PosOverflow;
+use std::ops::Add;
 use tower_http::classify::GrpcFailureClass::Error;
 use tower_http::cors::{Any, CorsLayer};
 
@@ -20,11 +22,17 @@ struct ErrorResponse {
 
 #[derive(Debug, PartialEq, Eq, Serialize, Clone)]
 struct Question {
-    question: HashMap<String, String>,
+    question: HashMap<String, Vec<String>>,
 }
 
 #[derive(Debug, PartialEq, Eq, Serialize, Clone)]
-struct QuestionResponse((String, String));
+struct PossibleAnswers {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+struct QuestionResponse((String, Vec<String>));
 #[tokio::main]
 async fn main() {
     let cors = CorsLayer::new()
@@ -98,20 +106,25 @@ async fn generate_questions() -> Question {
     let mut question = Question {
         question: Default::default(),
     };
+
     while question.question.len() < 10 {
         let x = rng.gen_range(1..20);
         let y = rng.gen_range(1..20);
+        let z = rng.gen_range(1..20);
+
+
         let question_string = format!("{}+{}", x, y);
         let answer = x + y;
         if !question.question.contains_key(&question_string) {
             // Insert the key-value pair into the HashMap
             question
                 .question
-                .insert(question_string.clone(), answer.to_string());
+                .insert(question_string.clone(), vec![answer.to_string(), x.to_string(), y.to_string(), z.to_string()]);
         } else {
             println!("Key already exists in the HashMap!");
         }
     }
+
     question
 }
 
@@ -121,9 +134,16 @@ async fn generate_custom_questions(mode: &str) -> Result<Vec<QuestionResponse>, 
     let mut question = Question {
         question: Default::default(),
     };
+    let mut answer_vec: Vec<PossibleAnswers> = Vec::new();
+    let mut possible_answers = PossibleAnswers {
+        x: Default::default(),
+        y: Default::default(),
+        z: Default::default(),
+    };
     while question.question.len() < 10 {
         let x = rng.gen_range(1..20);
         let y = rng.gen_range(1..20);
+        let z = rng.gen_range(1..20);
         let (answer, question_string) = match mode {
             "add" => (x + y, format!("{}+{}", x, y)),
             "subtract" => (x - y, format!("{}-{}", x, y)),
@@ -135,15 +155,24 @@ async fn generate_custom_questions(mode: &str) -> Result<Vec<QuestionResponse>, 
         };
         if !question.question.contains_key(&question_string) {
             // Insert the key-value pair into the HashMap
+            possible_answers.x = x;
+            possible_answers.y = y;
+            possible_answers.z = z;
             question
                 .question
-                .insert(question_string.clone(), answer.to_string());
+                .insert(question_string.clone(), vec![answer.to_string(), x.to_string(), y.to_string(), z.to_string()]);
+
+
+            answer_vec.push(possible_answers.clone());
+            let _ = answer_vec.iter().filter(|num| num.x == answer || num.y == answer || num.z == answer);
         } else {
             println!("Tried to enter a duplicate key.");
         }
     }
     for (key, value) in &question.question {
-        question_vec.push(QuestionResponse((key.to_string(), value.to_string())));
+        question_vec.push(QuestionResponse((key.to_string(), value.to_vec())));
     }
+    println!("{:?}", answer_vec);
+    println!(" {:?}", question_vec);
     Ok(question_vec)
 }
