b2efadf0859ea45c241b0491923a34117304dfb0
john <jhopkin@protonmail.com>
2023-10-30 09:39:01 +1000

    removed an unnecessary raoute and did a refactor

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,14 @@ struct ErrorResponse {
 
 #[derive(Debug, PartialEq, Eq, Serialize, Clone)]
 struct Question {
-    question: HashMap<String, Vec<String>>,
+    question: String,
+    answer: String,
+    choices: PossibleAnswers,
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+struct Questions {
+    question_vec: Vec<Question>,
 }
 
 #[derive(Debug, PartialEq, Eq, Serialize, Clone)]
@@ -45,7 +52,6 @@ async fn main() {
     let app = Router::new()
         .fallback(fallback)
         .route("/", get(handler))
-        .route("/maths_addition", get(return_questions))
         .route("/maths_problems", get(return_maths_questions))
         .layer(cors);
 
@@ -65,14 +71,14 @@ async fn fallback() -> String {
     "Didnt match any route".into()
 }
 
-async fn return_questions() -> Json<Question> {
-    let res = generate_questions().await;
-    Json(res)
-}
+// async fn return_questions() -> Json<Questions> {
+//     let res = generate_questions().await;
+//     Json(res)
+// }
 
 async fn return_maths_questions(
     operation: Query<MathOperation>,
-) -> Result<Json<Vec<QuestionResponse>>, Json<ErrorResponse>> {
+) -> Result<Json<Questions>, Json<ErrorResponse>> {
     let op = operation.0;
     match op.operation.as_str() {
         "add" => (),
@@ -91,7 +97,7 @@ async fn return_maths_questions(
     println!("query param is: {:?}", op);
     let res = generate_custom_questions(op.operation.as_str()).await;
     match res {
-        Ok(question) => Ok(Json(question)),
+        Ok(question) => Ok(question),
         Err(_) => {
             let error_response = ErrorResponse {
                 error: "Failed to generate questions".to_string(),
@@ -101,78 +107,41 @@ async fn return_maths_questions(
     }
 }
 
-async fn generate_questions() -> Question {
+async fn generate_custom_questions(mode: &str) -> Result<Json<Questions>, axum::Error> {
     let mut rng = rand::thread_rng();
     let mut question = Question {
         question: Default::default(),
+        answer: Default::default(),
+        choices: PossibleAnswers{
+            x: 0,
+            y: 0,
+            z: 0,
+        } ,
     };
+    let mut questions = Questions {
+        question_vec: vec![],
+    };
+    let mut pos_answers = PossibleAnswers { x: 0, y: 0, z: 0 };
 
-    while question.question.len() < 10 {
+    while questions.question_vec.len() < 10 {
         let x = rng.gen_range(1..20);
         let y = rng.gen_range(1..20);
-        let z = rng.gen_range(1..20);
-
 
         let question_string = format!("{}+{}", x, y);
         let answer = x + y;
-        if !question.question.contains_key(&question_string) {
-            // Insert the key-value pair into the HashMap
-            question
-                .question
-                .insert(question_string.clone(), vec![answer.to_string(), x.to_string(), y.to_string(), z.to_string()]);
-        } else {
-            println!("Key already exists in the HashMap!");
-        }
+
+        question.question = question_string.to_string();
+        question.answer = answer.to_string();
+
+            pos_answers.x = rng.gen_range(-20..answer) + rng.gen_range(answer..answer + 5);
+            pos_answers.y = rng.gen_range(-10..answer-5) + rng.gen_range(answer..answer + 7);
+            pos_answers.z = rng.gen_range(-5..answer-7) + rng.gen_range(answer..answer + 9);
+
+
+        question.choices = pos_answers.clone();
+
+        questions.question_vec.push(question.clone());
     }
 
-    question
-}
-
-async fn generate_custom_questions(mode: &str) -> Result<Vec<QuestionResponse>, axum::Error> {
-    let mut question_vec: Vec<QuestionResponse> = Vec::new();
-    let mut rng = rand::thread_rng();
-    let mut question = Question {
-        question: Default::default(),
-    };
-    let mut answer_vec: Vec<PossibleAnswers> = Vec::new();
-    let mut possible_answers = PossibleAnswers {
-        x: Default::default(),
-        y: Default::default(),
-        z: Default::default(),
-    };
-    while question.question.len() < 10 {
-        let x = rng.gen_range(1..20);
-        let y = rng.gen_range(1..20);
-        let z = rng.gen_range(1..20);
-        let (answer, question_string) = match mode {
-            "add" => (x + y, format!("{}+{}", x, y)),
-            "subtract" => (x - y, format!("{}-{}", x, y)),
-            "divide" => (x / y, format!("{}/{}", x, y)),
-            "multiply" => (x * y, format!("{}*{}", x, y)),
-            _ => {
-                return Err(axum::Error::new("Could not generate the questions - we expect either add, subtract, divide or multiply"))
-            }
-        };
-        if !question.question.contains_key(&question_string) {
-            // Insert the key-value pair into the HashMap
-            possible_answers.x = x;
-            possible_answers.y = y;
-            possible_answers.z = z;
-            question
-                .question
-                .insert(question_string.clone(), vec![answer.to_string(), x.to_string(), y.to_string(), z.to_string()]);
-
-
-            answer_vec.push(possible_answers.clone());
-            let _ = answer_vec.iter().filter(|num| num.x == answer || num.y == answer || num.z == answer);
-        } else {
-            println!("Tried to enter a duplicate key.");
-        }
-    }
-    for (key, value) in &question.question {
-        question_vec.push(QuestionResponse((key.to_string(), value.to_vec())));
-    }
-    println!("{:?}", answer_vec);
-    println!(" {:?}", question_vec);
-    Ok(question_vec)
+    Ok(Json(questions))
 }
